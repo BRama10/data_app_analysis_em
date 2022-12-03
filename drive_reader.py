792317e076cd0a9ac2e9f6c007be8e4887da289e
BRama10 <grand.hunter.dark.15@gmail.com>
2022-12-03 16:47:52 -0500

    Commented File

--- drive_reader.py
+++ drive_reader.py
@@ -22,12 +22,15 @@ class DriveAPI:
     
       
     # Define the scopes
+    #note that was overwritten when I deployed to webserver by client-server OAuth2 relationship
+    #this may pose problems later but I haven't ran into any yet
     SCOPES = ['https://www.googleapis.com/auth/drive']
 
-    
-  
+        
     def __init__(self, creds):
         self.file_list = []
+        
+        #credentials for user account
         self.creds = creds
   
         # Connect to the API service
@@ -46,8 +49,11 @@ class DriveAPI:
         #print(*items, sep="\n", end="\n\n")
   
     def FileDownloadNonWorkspace(self, file_id, file_name):
+        #calls request method specific to non-WorkSpace files (excel, pdf, jpg, etc)
         request = self.service.files().get_media(fileId=file_id)
         #request = self.service.files().export_media(fileId=file_id, mimeType='application/pdf')
+        
+        #creates a a bytes object to write raw file to
         fh = io.BytesIO()
         # Initialise a downloader object to download the file
         downloader = MediaIoBaseDownload(fh, request, chunksize=204800)
@@ -68,12 +74,17 @@ class DriveAPI:
             # Return False if something went wrong
             raise Exception
 
+    #note that there are a lot of print statements | this method is used to download our survey results
+    #I ran into many problems regarding authentication so I added debugger statements to find out where problems went wrong
+    #they only print to server however, so users aren't impacted by these
     def FileDownloadWorkspace(self, file_id, file_name):
-
+        #request to download G Workspace files (such as docs, sheets, etc)
         request = self.service.files().export_media(fileId=file_id, mimeType='text/csv')
         print("Stage 1 Done")
         fh = io.BytesIO()
         print("Stage 2 Done")
+        
+        
         # Initialise a downloader object to download the file
         downloader = MediaIoBaseDownload(fh, request, chunksize=204800)
         done = False
@@ -84,9 +95,12 @@ class DriveAPI:
                 status, done = downloader.next_chunk()
             print("Stage 4 Done")
             fh.seek(0)
-            #fn = os.path.dirname(__file__) + "nf.file"
+            
+            #fn = os.path.dirname(__file__) + "nf.file" | don't worry about this line
             # Write the received data to the file
             print("Stage 5 Done")
+            
+            #write the downloaded data to an actual file (bytes)
             with open('/tmp/datafile.file', 'wb') as f:
                 shutil.copyfileobj(fh, f)
   
@@ -99,7 +113,8 @@ class DriveAPI:
             print("File Downloaded")
         
     
-
+#connecting to DriveAPI requires both file id (unaccessible to user without extra work, and file name
+#this function makes process easy by scraping Drive, automatically finding id based on name
 class Scraper:
     def __init__(self):
         #self.lst = lst
@@ -108,6 +123,7 @@ class Scraper:
     def setList(self, lst):
         self.lst = lst
 
+    #find file by name
     def findByName(self,name):
         while len(self.lst) > 0:
             if(name == list(self.lst[0].values())[1]):
@@ -116,7 +132,7 @@ class Scraper:
             else:
                 del self.lst[0]
         
-        
+    #finds file by id
     def findByID(self,ID):
         while len(self.lst) > 0:
             if(ID == list(self.lst[0].values())[0]):
@@ -125,23 +141,31 @@ class Scraper:
             else:
                 del self.lst[0]
 
+    #gets id-value (name) pair if clean = True, otherwise just gets id
     def getValue(self, clean = False):
         if self.value != None and clean == False:
             return self.value
         elif clean == True:
             return tuple(self.value.values())
         return False
-
+      
+    #prints value (name) | used for debugging
     def printValue(self):
         if self.getValue() != False:
             print(self.getValue())
         else:
             print("No Value Found")
-
+            
+            
+#function that is called by main.py , connects to GDrive and downloads file
+#vars val = name of file, c = credentials for Google Account
 def callbackFunction(val, c):
+  
+    #creates DriveAPI & Scraper object
     obj = DriveAPI(c)
     scraper = Scraper()
   
+    #tries downloading files nonWorkSpace file.... if it doesn't work (meaning file is WorkSpace), then downloads as WorkSpace file
     try:
         k = val
             
@@ -154,7 +178,7 @@ def callbackFunction(val, c):
         if(check1 == False or check2 == False):
             raise NameError
         obj.FileDownloadNonWorkspace(scraper.getValue(clean = True)[0], scraper.getValue(clean = True)[1])
-            
+  
     except NameError:
         print("File Name Incorrect, Please Try Again")
     except:
