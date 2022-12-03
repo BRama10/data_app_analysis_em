81a181a51a321440782f4d4a534a4ab4f1f8ec9c
BRama10 <grand.hunter.dark.15@gmail.com>
2022-12-03 16:53:32 -0500

    Update db_html.py

--- db_html.py
+++ db_html.py
@@ -6,10 +6,11 @@ def html_tables():
     #path = str(Path(__file__).parent.absolute())+ '\\templates'
     path = '/tmp/templates/'
 
-
+    #writes data & contact tables into html files, which are then displayed to the user when selected
+    #strategy is => download from csv, convert to html, save as html in new location
     df1 = pd.read_csv('/tmp/cleaned_data.csv', index_col=0)
     df2 = pd.read_csv('/tmp/contact_data.csv', index_col=0)
-
+ 
     f = open(path + 'data.html','w')
     result = df1.to_html()
     f.write(result)
