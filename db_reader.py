363fa86e40e642a746cf8ba8e5d2a9145562512c
BRama10 <grand.hunter.dark.15@gmail.com>
2022-12-03 16:52:00 -0500

    Update db_reader.py

--- db_reader.py
+++ db_reader.py
@@ -2,20 +2,24 @@ from pathlib import Path
 import sqlite3
 import pandas as pd
 
+#function called from main.py
 def main_db():
 
+    #creates database
     Path('/tmp/em_volunteer_survey.db').touch()
 
+    #creates connection and cursor for database
     conn = sqlite3.connect('/tmp/em_volunteer_survey.db')
     cur = conn.cursor()
 
+    #read data & contact tables into database
     data = pd.read_csv('/tmp/cleaned_data.csv')
     contact = pd.read_csv('/tmp/contact_data.csv')
-
     data.to_sql('data', conn, if_exists='replace', index = True)
     contact.to_sql('contact', conn, if_exists='replace', index = True)
 
 
+    #disregard below lines -------
     #cur.execute("ALTER TABLE contact DROP COLUMN 'Unnamed: 0';")
     #conn.commit()
     #cur.execute("ALTER TABLE data DROP COLUMN 'Unnamed: 0';")
